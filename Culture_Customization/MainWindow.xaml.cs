2e1634dcbdc6f546c7780037f24ea3e3a622765f
Hemalatha <hemalathavinai@gmail.com>
2020-12-14 23:10:38 +0530

    Sample included

---
+++ Culture_Customization/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GettingStartedDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RangeNavigator_LowerBarLabelsCreated(object sender, Syncfusion.UI.Xaml.Charts.LowerBarLabelsCreatedEventArgs e)
+        {
+            var lowerBarLabels = e.LowerBarLabels;
+            foreach (var item in lowerBarLabels)
+            {
+                var actualContent = DateTime.Parse(item.Content.ToString());
+                (item as RangeNavigatorLabel).Content = actualContent.ToString("hhtt", new CultureInfo("zh-hk"));
+            }
+        }
+    }
+
+    
+}
---
+++ Culture_Customization/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStartedDemo
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.StockPriceDetails = new ObservableCollection<Model>();
+            DateTime date = new DateTime(2012, 1, 1);
+            Random rd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                this.StockPriceDetails.Add(new Model() { XValue = date.AddHours(i), YValue = rd.Next(100, 900) });
+            }
+
+        }
+
+        public ObservableCollection<Model> StockPriceDetails { get; set; }
+
+    }
+}
